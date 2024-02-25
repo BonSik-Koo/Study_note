9198518ebfc707c3b72a023bfbf953e2f758e956
BonSik <rnqhstlr2297@gmail.com>
2024-02-25 22:23:02 +0900

    Java 멀티 스레드 가시성 학습 코드

---
+++ spring/src/main/java/com/example/spring/java_concurrent/visibility/Visibility.java
@@ -0,0 +1,19 @@
+package com.example.spring.java_concurrent.visibility;
+
+public class Visibility {
+    public static volatile boolean runningFlag = false;
+    public static class thread extends Thread {
+        @Override
+        public void run() {
+            while (!runningFlag) {}
+            System.out.println("탈출 성공");
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        thread thread1 = new thread();
+        thread1.start();
+        Thread.sleep(3000);
+        runningFlag = true;
+        thread1.join();
+    }
+}
